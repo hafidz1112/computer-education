d3a4a00d72e37c91f046590a6155d3d13c2d8ff3
hafidz1112 <153370411+hafidz1112@users.noreply.github.com>
2025-06-10 13:25:40 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hidden");
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: 2, // default di desktop
+
+        breakpoints: {
+            0: {
+                slidesPerView: 1 // untuk layar kecil, misalnya HP
+            },
+            768: {
+                slidesPerView: 3 // untuk layar tablet ke atas
+            }
+        }
+    });
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.slider-pengurus', {
+        wrapperClass: 'slider-pengurus-wrapper',
+        slideClass: 'slider-pengurus-slide',
+        loop: true,
+        navigation: {
+            nextEl: '.slider-pengurus .swiper-button-next',
+            prevEl: '.slider-pengurus .swiper-button-prev',
+        },
+        pagination: {
+            el: '.slider-pengurus .swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: 2,
+        spaceBetween: 20,
+        breakpoints: {
+            0: {
+                slidesPerView: 1
+            },
+            768: {
+                slidesPerView: 4 
+            }
+        }
+    });
+});
